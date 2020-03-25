e8e2a1835ce08fb94abb6ca791819b86a6250967
Bartlomiej.gladys <bartlomiej.gladys@euvic.pl>
2020-03-25 23:26:25 +0100

    add fake response time

--- src/test/index.ts
+++ src/test/index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export default app => {
   app.get("/test", async (req: Request, resp: Response) => {
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    for(let i =0; i< 30000000; i++) {}
+    for(let i =0; i< 60000000; i++) {}
     resp.json({ ip });
   });
 };
